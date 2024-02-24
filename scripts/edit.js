ba1205e37aa6805583b1c5a2fe3f30c653426281
romac1314 <68718248+romac1314@users.noreply.github.com>
2024-02-23 22:06:24 -0500

    fixed line 34 for update

--- scripts/edit.js
+++ scripts/edit.js
@@ -31,7 +31,7 @@ async function updateCourse(){
     const response = await fetch("http://localhost:3000/api/courses/" + courseID,{
         method: "PUT",
         headers: {
-           "Content-Type" : "applications/json"
+           "Content-Type" : "application/json"
         },
         body: JSON.stringify(course)
     })
