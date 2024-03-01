790c79ed73286daf30d0415e027f3e36c98a2175
allisonurquhart <allison.urquhart.dev@gmail.com>
2024-02-29 21:21:26 -0500

    student signup form

--- scripts/studentScript.js
+++ scripts/studentScript.js
@@ -1,11 +1,13 @@
 addEventListener("DOMContentLoaded", async function() {
     const response = await fetch ("http://localhost:3000/api/courses")
     const courses = await response.json();
+    document.querySelector("#registerBtn").addEventListener("click", registerClicked)
 
     let checkBoxHtml = ""
+    let registeredHtml = ""
 
     for (let course of courses){
-        checkBoxHtml += `<li><input type="checkbox" value="${course.course_name}" id="course"><label for="course">&nbsp&nbsp${course.course_name}</label></li>`
+        checkBoxHtml += `<li><input type="checkbox" value="${course.course_name}" id="courseCheck"><label for="courseCheck">&nbsp&nbsp${course.course_name}</label></li>`
     }
 
     document.querySelector("#studentForm").innerHTML = checkBoxHtml
@@ -15,6 +17,15 @@ addEventListener("DOMContentLoaded", async function() {
     }
     else{
         document.querySelector("studentP").innerHTML = "There are no classes available for registration."
+    }
+
+    async function registerClicked(){
+
+        if (document.querySelector("#courseCheck").checked){
+        registeredHtml += `<li>${course.course_name} - ${course.subject_area} - ${course.cred_number}</li>`
+        }
+
+        document.querySelector("#studentSignedUp").innerHTML = registeredHtml
     }
     
 })
