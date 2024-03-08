cae36b4e9b51c825da463a5cd6ed0192acdeea60
allisonurquhart <allison.urquhart.dev@gmail.com>
2024-03-07 20:03:48 -0500

    styled student sign up and added extra information

--- scripts/studentScript.js
+++ scripts/studentScript.js
@@ -24,13 +24,14 @@ addEventListener("DOMContentLoaded", async function() {
 async function registerClicked() {
     const response = await fetch("http://localhost:3000/api/courses");
     const courses = await response.json();
-    let registeredHtml = "";
+    let registeredHtml = ""
     let form = document.querySelector("#studentForm");
     let checkboxes = form.querySelectorAll("input[type=checkbox]");
 
     checkboxes.forEach((checkbox, index) => {
         if (checkbox.checked) {
             registeredHtml += `<li>${courses[index].course_name} - ${courses[index].subject_area} - ${courses[index].cred_number}</li>`;
+            document.querySelector("#yourClasses").innerHTML = "These are the classes you are currently registered for."
         }
     });
 
